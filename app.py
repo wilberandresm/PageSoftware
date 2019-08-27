bfd36119ddb013c201cc8b7aa38ac54963d96d08
wilberandresm <36487940+wilberandresm@users.noreply.github.com>
2019-08-27 16:17:31 -0500

    third commit
    docker

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def lista2():
     return jsonify({'listado':[dict(row)for row in resultado]})
  
 if __name__ == "__main__":
-    app.run(port=5000,debug=True)
+    app.run(debug= True, host= '0.0.0.0', port=80)
