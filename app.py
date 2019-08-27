b56ed25362aea4be4ee8c5a0256162d8dbfdca53
wilberandresm <wilberandresm@hotmail.com>
2019-08-26 22:15:15 -0500

    PageWeb
    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,escape,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///databases/estudiantes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+app.secret_key='mysecretkey'
+
+@app.route('/',methods= ['GET'])
+def hello_world(): 
+    return render_template('inicio.html')
+
+@app.route('/users/list',methods= ['GET'])
+def lista(): 
+    datos=db.engine.execute('select * from estudiantes')
+    return render_template('lista.html',estudents=datos)
+
+@app.route('/api/v1/users/')
+def lista2():
+    resultado=db.engine.execute('select * from estudiantes')
+    return jsonify({'listado':[dict(row)for row in resultado]})
+ 
+if __name__ == "__main__":
+    app.run(port=5000,debug=True)
